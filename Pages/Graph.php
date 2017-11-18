0d496bf78c75cbba038b888b541f56b1cf4e6c4f
eHoward1996 <evan.howard96@gmail.com>
2017-11-18 08:52:23 -0500

    Added Graph and FusionChart Library

---
+++ Pages/Graph.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<!-- include links to CSS and JS Files -->
+		<script><?php include 'mainScripts.js'; ?></script>
+		<style> <?php include 'mainStyle.css';  ?></style>
+		<?php include '../Misc/FusionCharts/fusioncharts.php'; ?>
+	</head>
+
+	<body>
+		<!--Div that will hold the pie chart-->
+		<div id="chart_div">
+			<?php
+
+			?>
+		</div>
+	</body>
+</html>
